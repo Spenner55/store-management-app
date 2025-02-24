9ed1d0a61e11764b6296f840e9e9f97686b27ae1
Spenner55 <82128897+Spenner55@users.noreply.github.com>
2025-02-24 14:28:09 -0700

    added unsaved files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,29 +4,38 @@ import Public from './components/Public';
 import DashLayout from './components/DashLayout';
 import EmployeesList from './features/employee/EmployeesList';
 import Login from './features/auth/Login';
+import RequireAuth from './features/auth/RequireAuth';
 import Home from './features/auth/Home';
+import { ROLES } from './config/roles';
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Public />}/>
+	return (
+		<Routes>
+			<Route path="/" element={<Layout />}>
+				<Route index element={<Public />}/>
 
-        <Route path='login' element={<Login/>}/>
+				<Route path='login' element={<Login/>}/>
 
-        <Route path="dash" element={<DashLayout />} >
+				<Route element={<RequireAuth allowedRoles={[ROLES.Employee, ROLES.Manager, ROLES.Admin]} />} >
+					<Route path="dash" element={<DashLayout />} >
 
-          <Route index element={<Home/>}/>
+					<Route index element={<Home/>}/>
 
-          <Route path='employees'>
-            <Route index element={<EmployeesList />} />
-          </Route>
+					<Route path='employees'>
+						<Route index element={<EmployeesList />} />
+					</Route>
 
-        </Route>
+					</Route>
+				</Route>
 
-      </Route>
-    </Routes>
-  );
+				<Route element={<RequireAuth allowedRoles={[ROLES.Manager, ROLES.Admin]} />} >
+
+				</Route>
+				
+
+			</Route>
+		</Routes>
+	);
 }
 
 export default App;
