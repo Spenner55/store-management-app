2e35a13fa9445c9eca291c80fc998691897448ab
Spenner55 <82128897+Spenner55@users.noreply.github.com>
2025-09-30 13:29:22 -0600

    added schedule endpoint with 2 GET, 1 PUT, 1 POST, 1 DELETE path

--- backend/routes/schedulesRoutes.js
+++ backend/routes/schedulesRoutes.js
@@ -4,13 +4,13 @@ const schedulesController = require('../controllers/schedulesController');
 const verifyJWT = require('../middleware/verifyJWT');
 const verifyRole = require('../middleware/verifyRoles');
 
-//router.use(verifyJWT);
+router.use(verifyJWT);
 
 router.get('/me', schedulesController.getMyShifts);
 router.get('/', schedulesController.getAllShifts);
 
-router.post('/', /*verifyRole('Manager', 'Admin'),*/ schedulesController.createNewShift);
-router.put('/:id', /*verifyRole('Manager', 'Admin'),*/ schedulesController.updateShift);
+router.post('/', verifyRole('Manager', 'Admin'), schedulesController.createNewShift);
+router.put('/:id', verifyRole('Manager', 'Admin'), schedulesController.updateShift);
 router.delete('/:id', verifyRole('Manager', 'Admin'), schedulesController.deleteShift);
 
 module.exports = router;
